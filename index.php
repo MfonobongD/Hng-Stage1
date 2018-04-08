fa039a8c7af80e278fcb2f0ceb210d13752a28d5
MfonobongD <umondiamfonobong@gmail.com>
2018-04-09 00:36:19 +0200

    added a new file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>First Task</title>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/bella.css">
+</head>
+<body >
+    <h1 class="header">Hotels.ng Internship 4.0</h1>
+    <p class="btn">Saturday,7th April 2018</p>
+</body>
+<p>
+<?php
+date_default_timezone_set("Africa/Lagos");
+echo "The time is " . date("h:i:sa");
+?>
+</html>
